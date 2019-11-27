34a9abe7bd73aa7347a386763e1606016a372c6f
ThiagoViBritannia <41394481+ThiagoViBritannia@users.noreply.github.com>
2019-11-26 21:42:16 -0400

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+if [ -s 30_os-prober ]
+then
+	mv 30_os-prober /etc/grub.d/
+else
+	echo "arquivo '30_os-probe' não encontrado ou vazio, instalação interrompida"
+fi
+
+if [ -s grub ]
+then
+	mv grub /etc/default/
+else
+	echo "arquivo 'grub' não encontrado ou vazio, instalação interrompida"
+fi
+
+
+
